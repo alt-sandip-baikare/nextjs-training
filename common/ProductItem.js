0aa4439a816c09f74b40b494693ae0f454f045f8
Sandip Baikare <sandip.baikare@altudo.co>
2022-11-21 14:57:24 +0530

    Added product details page

--- common/ProductItem.js
+++ common/ProductItem.js
@@ -9,17 +9,17 @@ function ProductItem({ product }) {
     return (
         <>
             <div className='col-md-3 py-3'>
-
                 <div class="card" >
                     <Image src={product.image} width="250" height="250" className="card-img-top" />
                     <div class="card-body">
+                     <span className='text-capitalize badge text-bg-dark text-white'>{product.category}</span>
                         <Link href={"/products/" + product.id}> <h5 class={ styles.heading + " card-title "}>{product.title}</h5> </Link>
                         <p class={styles.product_short_desc + " card-text"}>{shortDescription}</p>
                     </div>
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item"> 
                             <strong className=''>Rating: </strong> {product.rating.rate} ({product.rating.count})
-                            <strong className='px-2'>Price: </strong> {product.price}
+                            <strong className='px-2'>Price: ₹ </strong> {product.price}
                         </li>                        
                     </ul>
                     <div class="card-body d-flex">
--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -1,67 +1,80 @@
 import { useRouter } from 'next/router'
 import ErrorPage from "next/error";
 import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
 
 
-function ProductSingle({product}) {
+function ProductSingle({ product }) {
   const router = useRouter()
   const { productId } = router.query
 
-  if (router.isFallback)  return <div>Loading...</div>;
-  
+  if (router.isFallback) return <div>Loading...</div>;
+
   if (!product) {
     return <ErrorPage statusCode={404} />;
   }
 
-
-
   return (
     <>
-    { 
-    error && 
-    <>
-    <p> Sorry there is something issue.... try again! </p>
-    </> 
-    }
-    <div className='container'>
-      <h1>{product.title}</h1>
-      <div className='product-details'>
-          <div className='description'>
-          {product.description}
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-6'>
+            <Image src={product.image} width="500" height="500" />
           </div>
+          <div className='col-md-6 '>
+            <div className='product-details pt-5'>
+              <h1>{product.title}</h1>
+              <div className='product-data'>
+                <p>
+                  <strong> Price: </strong>
+                  <span> ₹ {product.price} </span>
+                </p>
+                <p> <strong>Product Category:</strong> <span className='text-capitalize'>{product.category}</span></p>
+                <p>
+                  <strong> Rating: </strong>
+                  <span> {product.rating.rate} </span>
+                  (<span title={"Total rating given " + product.rating.count}> {product.rating.count} </span>)
+                </p>
+                <a href="#" class="card-link btn btn-primary ">Add to Watchlist</a>
+                <a href="#" class="card-link btn btn-primary mx-3">Add to Cart</a>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=''>
+          <div className='description'>
+            <h3>Product Description</h3>
+            {product.description}
+          </div>
+        </div>
+
       </div>
-    </div>
     </>
   )
 }
 
 export default ProductSingle
 
-export const getStaticProps = async ({params}) =>{
-    
-    const response = await fetch(`https://fakestoreapi.com/products/${params.productId}/`);
-    const product = await response.json();
-    console.log(response)
-    // if(null == product){
-    //   return {
-    //     props: {
-    //       error: true
-    //     }
-    //   }
-    // }
-    return {
-        props: {
-            product,
-            error: false
-        }
+export const getStaticProps = async ({ params }) => {
+  console.log(params.productId)
+  const response = await fetch(`https://fakestoreapi.com/products/${params.productId}/`);
+  const product = await response.json();
+  console.log(response)
+  return {
+    props: {
+      product: product || null,
+      error: false
     }
+  }
 }
 
-export const getStaticPaths = async ({params}) => {
+export const getStaticPaths = async ({ params }) => {
 
   return {
-      paths: [],
-      // paths: [], //indicates that no page needs be created at build time
-      fallback: true, //indicates the type of fallback
+    paths: [],
+    // paths: [], //indicates that no page needs be created at build time
+    fallback: true, //indicates the type of fallback
   }
 }
